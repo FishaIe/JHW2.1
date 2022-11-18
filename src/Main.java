81159fb3930708462b989d1f532acf2dc7afd8b1
Fishale <fishale2500@gmail.com>
2022-11-18 14:13:31 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long cost = 26690; //ticket cost
+        long mili = cost/20; //mili bonus
+
+        System.out.println(mili);
+    }
+}
