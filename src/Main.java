aa4f47bfd25abb2d27628d734ffa4014416871e6
Fishale <fishale2500@gmail.com>
2022-11-21 15:14:52 +0700

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         long cost = 26690; //ticket cost
-        long mili = cost/20; //mili bonus
+        long mili = cost / 20; //mili bonus
 
         System.out.println(mili);
     }
